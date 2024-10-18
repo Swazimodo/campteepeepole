a07018dad0e98c5c92fbdfb48825c06bed41afbe
Kanuea <kanuea@ymail.com>
2024-10-17 20:52:43 -0600

    feat: add content to home and about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,7 @@
 export default function Page() {
-  return <h1>Hello, about Page!</h1>
+  return <div>
+  <h1>Hello, about Page!</h1>
+  <h2>Facility History & Overview</h2>
+  <p>Camp Teepee Pole began in the early 1960’s when a couple of Christian Service Brigade groups recognized the beautiful setting to teach boys outdoor skills by ‘doing’. They came out regularly with their tents, gear and food and spent weekends enjoying the great outdoors along with all its challenges.</p>
+</div>
 }

2e91db30a25470d935700c8eff65301eb648a02e
Swazimodo <Swazimodo@users.noreply.github.com>
2025-03-04 20:33:37 -0700

    fix: prevent infinite render loop (#12)
    Co-authored-by: Swazimodo <swn@duck.com>

--- src/components/clientSideMemoHook.tsx
+++ src/components/clientSideMemoHook.tsx
@@ -1,10 +1,16 @@
 import { DependencyList, useEffect, useState } from "react"
 
-export const useClientSideMemo = <T,>(func: () => T, deps: DependencyList): T | null => {
+/**
+ * Acts like the useMemo hook but will only evaluate client side.
+ * Useful for preventing SSR from running window related code.
+ * @param calculateValue function containing the client side code
+ * @param deps The list of all reactive values referenced inside of the `calculateValue` code
+ * @returns result from executing `calculateValue`
+ */
+export const useClientSideMemo = <T,>(calculateValue: () => T, deps: DependencyList): T | null => {
   const [value, setValue] = useState<T | null>(null)
-  Object.assign([], deps)
   useEffect(() => {
-    setValue(func())
-  }, [func, ...deps])
+    setValue(calculateValue())
+  }, [setValue, calculateValue, ...deps])
   return value
 }

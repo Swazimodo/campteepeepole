c285cb6e08c58ef58655ece846a01a37b026a6bf
Swazimodo <swn@duck.com>
2025-07-15 08:29:19 -0600

    feat: get s3 images and group with thumbs

--- src/components/awsWrappers/s3.tsx
+++ src/components/awsWrappers/s3.tsx
@@ -5,8 +5,7 @@ import { useSiteConfig } from "@/components/config"
 export const useS3Client = () => {
   const awsConfig = useSiteConfig().awsConfig
   return new S3Client({
-    region: "ap-northeast-1",
-    // region: awsConfig.region,
+    region: awsConfig.region,
     credentials: {
       secretAccessKey: "MOCK",
       accessKeyId: "MOCK",
@@ -19,18 +18,61 @@ export const useS3Client = () => {
   })
 }
 
-export const useS3FileKeys = async () => {
+interface S3Image {
+  url: string,
+  thumbUrl?: string,
+  alt: string,
+}
+
+/**
+ * Lists images from a directory in AWS S3. Images will be matched up with the thumbnail if available.
+ * @param path S3 directory to pull images from
+ */
+export const useS3DirectoryImages = async (path: string) => {
+  const { bucketName } = useSiteConfig().awsConfig
+  const s3Client = useS3Client()
   return await useMemo(async () => {
     try {
-      const { bucketName, galleryPath } = useSiteConfig().awsConfig
-      const s3Client = useS3Client()
-      const command = new ListObjectsV2Command({ Bucket: bucketName, Prefix: galleryPath })
-      return (await s3Client.send(command)).Contents?.map(x => x.Key).filter(x => !!x) as string[]
+      const command = new ListObjectsV2Command({ Bucket: bucketName, Prefix: path })
+      return Object.values((await s3Client.send(command)).Contents
+        ?.filter(x => !!x.Key && !!x.Size && x.Size > 0)                                  // filter out invalid files
+        .map(x => x.Key)                                                                  // keep only the file keys
+        .reduce<{ [key: string]: S3Image }>(reduceImageList(bucketName), {}) ?? {})        // group images and thumbnails
+        .filter(x => !!x.url)                                                             // filter out thumbnails without a full sized image
     }
     catch (ex) {
       console.error(ex)
     }
     return []
-  }, [])
+  }, [path])
 }
 
+/**
+ * Groups full sized images with the thumbnails. Thumbnails are identified by a name starting with `thumb_`
+ */
+const reduceImageList = (bucketName: string) => (accumulator: { [key: string]: S3Image }, currentValue?: string): { [key: string]: S3Image } => {
+  if (!currentValue)
+    return accumulator
+
+  const fileName = currentValue.substring(currentValue.lastIndexOf('/') + 1)
+  const isThumb = fileName.startsWith('thumb_')
+  const keyName = isThumb ? fileName : fileName
+
+  let entry = accumulator[keyName]
+  if (!entry) {
+    entry = {
+      url: "",
+      thumbUrl: undefined,
+      alt: keyName
+    }
+    accumulator[keyName] = entry
+  }
+
+  if (isThumb) {
+    entry.thumbUrl = `https://${bucketName}.s3.amazonaws.com/${currentValue}`
+  } else {
+    entry.url = `https://${bucketName}.s3.amazonaws.com/${currentValue}`
+  }
+
+  return accumulator
+}
--- src/components/gallery/gallery.tsx
+++ src/components/gallery/gallery.tsx
@@ -1,11 +1,18 @@
-import { useS3FileKeys } from "@/components/awsWrappers/s3"
 import { FC } from "react"
+import { useS3DirectoryImages } from "@/components/awsWrappers/s3"
+import { useSiteConfig } from "@/components/config"
 
 
 export const Gallery: FC = async () => {
-  const files = await useS3FileKeys()
+  const { galleryPath } = useSiteConfig().awsConfig
+  const files = await useS3DirectoryImages(galleryPath)
   if (!files.length) {
     return null
   }
-  return <div>Gallery: {JSON.stringify(files)}</div>
+  return <div>
+    Gallery:
+    {files.map(file => <div key={file.url}>
+      <img src={file.url} alt={file.alt} />
+    </div>)}
+  </div>
 }

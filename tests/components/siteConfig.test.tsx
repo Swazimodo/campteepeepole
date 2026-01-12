25f25d8ea573c594cf953fd0a2b74e3bdcee49b3
Swazimodo <swn@duck.com>
2026-01-11 22:07:26 -0700

    chore: test site config

--- tests/components/siteConfig.test.tsx
+++ tests/components/siteConfig.test.tsx
@@ -1,13 +1,38 @@
-import { useSiteConfig } from "@/components/siteConfig";
-
+beforeEach(() => {
+  jest.resetModules();
+});
 
 test("getSiteConfig_withUndefinedCamp_shouldDefaultToTpp", () => {
-  jest.mock('src/static/siteConfig.json', () => ({
-    settings: 'someSetting'
-  }), { virtual: true });
+  jest.doMock('@/static/siteConfig.json', () => ({
+    settings: 'foo'
+  }));
   const { useSiteConfig } = require('@/components/siteConfig');
 
   const siteConfig = useSiteConfig();
 
   expect(siteConfig.camp).toBe("campTeepeePole");
+});
+
+test("getSiteConfig_withCampTpp_shouldBeTpp", () => {
+  const camp = 'campTeepeePole'
+  jest.doMock('@/static/siteConfig.json', () => ({
+    camp
+  }));
+  const { useSiteConfig } = require('@/components/siteConfig');
+
+  const siteConfig = useSiteConfig();
+
+  expect(siteConfig.camp).toBe(camp);
+});
+
+test("getSiteConfig_withCampCherith_shouldBeCampCherith", () => {
+  const camp = 'campCherith'
+  jest.doMock('@/static/siteConfig.json', () => ({
+    camp
+  }));
+  const { useSiteConfig } = require('@/components/siteConfig');
+
+  const siteConfig = useSiteConfig();
+
+  expect(siteConfig.camp).toBe(camp);
 });
